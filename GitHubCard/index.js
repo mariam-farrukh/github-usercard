1860b79f75290b5b9012954eecd24e1dd0370622
mariam-farrukh <50977034+mariam-farrukh@users.noreply.github.com>
2019-07-11 15:35:20 -0500

    Added follower cards, completed mvp

--- GitHubCard/index.js
+++ GitHubCard/index.js
@@ -34,7 +34,20 @@ axios.get(`https://api.github.com/users/mariam-farrukh`)
           user, and adding that card to the DOM.
 */
 
-const followersArray = []
+const followersArray = ['tetondan', 'dustinmyers', 'justsml', 'luishrd', 'bigknell']
+
+followersArray.forEach(follower => {
+  axios.get(`https://api.github.com/users/${follower}`)
+  .then(data => {
+    const user = data.data;
+    const container = document.querySelector('.cards');
+    container.appendChild(gitHubCards(user));
+    console.log("Works", data.data);
+  })
+  .catch(error => {
+    console.log("Oh no!", error)
+  })
+})
 
 /* Step 3: Create a function that accepts a single object as its only argument,
           Using DOM methods and properties, create a component that will return the following DOM element:
